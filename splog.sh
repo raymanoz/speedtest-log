c78090233d77f6d7c1313363c80c942d86b94370
Raymond Barlow <rbarlow@raymanoz.com>
2018-06-06 23:25:53 +1000

    ensure data directory exists, before use

--- splog.sh
+++ splog.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dir="$( cd "$( dirname "$0" )" && pwd )"
 data=${dir}/data
+mkdir -p ${data}
 
 command -v node >/dev/null 2>&1 || { echo "This script requires 'node'. Please install."; exit 1; }
 command -v speedtest-cli >/dev/null 2>&1 || { echo "This script requires 'speedtest-cli'. Please install."; exit 1; }
