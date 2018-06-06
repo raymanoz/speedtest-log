9dec0125aba200d0da1d710fc65edbe80da4ffbb
Raymond Barlow <rbarlow@raymanoz.com>
2018-06-06 23:23:24 +1000

    put logs in data directory. remove empty files

--- splog.sh
+++ splog.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 dir="$( cd "$( dirname "$0" )" && pwd )"
+data=${dir}/data
 
 command -v node >/dev/null 2>&1 || { echo "This script requires 'node'. Please install."; exit 1; }
 command -v speedtest-cli >/dev/null 2>&1 || { echo "This script requires 'speedtest-cli'. Please install."; exit 1; }
 
 d=$(node -e "console.log(new Date().toJSON());")
-speedtest-cli --json > ${dir}/${d}.log 2> ${dir}/${d}.err
+speedtest-cli --json > ${data}/${d}.log 2> ${data}/${d}.err
+find ${data} -type f -empty -delete
--- summarize.sh
+++ summarize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+dir="$( cd "$( dirname "$0" )" && pwd )"
+data=${dir}/data
 
 command -v jq >/dev/null 2>&1 || { echo "This script requires 'jq'. Please install."; exit 1; }
 
 function extract {
-  for f in ./*.log
+  for f in ${data}/*.log
   do
     cat $f | jq '{timestamp: .timestamp, ping: .ping, down: .download, up: .upload}' && echo ","
   done
